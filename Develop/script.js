44221ceffccde8a29f2252d24f694ea1108986a5
miss-mad <mmlh.harris@gmail.com>
2022-08-03 15:59:25 -0400

    formatting scheduler to be more aesthetically pleasing

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,9 @@
+// Creates the variable today to retrieve the current day of the week, month, day of the month, and year using moment.js, then displays that info using jQuery by matching it to the same HTML ID
 var today = moment();
 $("#currentDay").text("Today is: " + today.format("dddd, MMMM Do, YYYY"));
+
+var now = moment();
+
+if () {
+
+}
