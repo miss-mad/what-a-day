95394a7d83687cd6500de1a0705845e9a9db93b5
miss-mad <mmlh.harris@gmail.com>
2022-08-03 23:08:39 -0400

    changed background colors of textareas (for event details) successfully with an if/else/if within a jQuery for each method

--- Develop/script.js
+++ Develop/script.js
@@ -2,29 +2,69 @@
 var today = moment();
 $("#currentDay").text("Today is: " + today.format("dddd, MMMM Do, YYYY"));
 
-var now = moment();
+var now = moment().hour();
+console.log(now);
 
-var scheduledHourMorning = document.getElementsByClassName("hour-morn");
+var eventDetails = $(".form-control");
+console.log(eventDetails);
+
+// var scheduledHourMorning = document.getElementsByClassName("hour-morn");
 // scheduledHourMorning.split("AM");
 
-var scheduledHourAfternoon = document.getElementsByClassName("hour-aft");
+// var scheduledHourAfternoon = document.getElementsByClassName("hour-aft");
 // scheduledHourAfternoon.split("PM");
 
-var both = scheduledHourMorning + scheduledHourAfternoon;
-console.log(both);
+// var both = scheduledHourMorning + scheduledHourAfternoon;
+// console.log(both);
 
-var eventBackgroundColor = $("#form-control");
+// don't need morn and aft - retrieve with hour id in parent div in html
+// var scheduledHour = $("#time8").attr("id").split("time").pop();
+// var scheduledHourString = scheduledHour.toString();
+// var scheduledHourStringSplit = scheduledHourString.split(",")
+// var scheduledHourNumber = scheduledHourString.split("time");
+// console.log(scheduledHour);
+// console.log(scheduledHourString);
+// console.log(scheduledHourStringSplit);
+// console.log(scheduledHourNumber);
 
-if (scheduledHourMorning && scheduledHourAfternoon < now) {
-  console.log("This is in the past");
-  eventBackgroundColor.addClass("past");
-} else if (scheduledHourMorning || scheduledHourAfternoon === now) {
-  console.log("This is the current hour");
-  eventBackgroundColor.addClass("present");
-} else {
-  console.log("This is in the future");
-  eventBackgroundColor.addClass("future");
-}
+var eventBackgroundColor = $("textarea");
+
+$(".row").each(function () {
+  var scheduledHour = $(this).attr("id").split("time").pop();
+  console.log(scheduledHour);
+
+  if (scheduledHour < now) {
+    console.log("This is in the past");
+    eventBackgroundColor.addClass("past");
+  } else if (scheduledHour === now) {
+    console.log("This is the current hour");
+    eventBackgroundColor.addClass("present");
+  } else {
+    console.log("This is in the future");
+    eventBackgroundColor.addClass("future");
+  }
+});
+
+// returns array of divs with form control class
+// console.log("eventBackgroundColor: ", eventBackgroundColor);
+
+// for loop instead to iterate over the textareas (array indexing [])
+// i -> so that if else statement can reference i for background color
+
+// for (var i = 0; i < eventDetails.length; i++) {
+//   console.log("this works 11 times");
+
+//   if (scheduledHour < now) {
+//     console.log("This is in the past");
+//     // eventBackgroundColor[i].addClass("past");
+//   } else if (scheduledHour === now) {
+//     console.log("This is the current hour");
+//     // eventBackgroundColor[i].addClass("present");
+//   } else {
+//     console.log("This is in the future");
+//     // eventBackgroundColor[i].addClass("future");
+//   }
+// }
 
 // var button = $(".btn")
 // button.on("click", function);
